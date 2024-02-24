64a7b82c29bf1c18238825b21edc918a8c2c135c
waarrdii <waarrdii@gmail.com>
2024-02-24 11:50:23 +0700

    binding data done

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 const app = new Vue({
     el : "#app",
     data : {
-        product : []
+      products : []
     },
     mounted() {
         // Fetch data produk dari WooCommerce API
-        axios.get(`https://api.escuelajs.co/api/v1/products`)
+        axios.get(`https://api.escuelajs.co/api/v1/products?offset=0&limit=50`)
           .then(response => {
             // Simpan data produk dalam state
             this.products = response.data
             console.log(this.products)
           })
-      }
-    
+      },
+
 })
